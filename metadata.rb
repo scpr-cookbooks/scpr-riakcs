f1c830ecfaf7f97b45c7fc8a806b8acf69a291d4
Eric Richardson <e@ericrichardson.com>
2016-02-03 14:14:00 -0500

    0.2.3: Relax dep constraints on scpr-consul and scpr-consul-haproxy

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'erichardson@scpr.org'
 license          'apache2'
 description      'Installs/Configures scpr-riakcs'
 long_description 'Installs/Configures scpr-riakcs'
-version          '0.2.2'
+version          '0.2.3'
 
-depends 'scpr-consul', "~> 0.1.25"
+depends 'scpr-consul', "~> 0.2"
 depends 'riak', "2.4.21"
 depends 'riak-cs', "2.2.11"
 
-depends 'scpr-consul-haproxy', "~> 0.4.2"
+depends 'scpr-consul-haproxy', "~> 0.5"
